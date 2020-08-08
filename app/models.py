43af586d8cf2d8e86ab99f5a0a0ccad6406b0b28
koudai9612 <koudai9612@yahoo.co.jp>
2020-08-08 15:38:55 +0900

    login title changed

--- app/models.py
+++ app/models.py
@@ -12,7 +12,6 @@ class Item(models.Model):
     https://docs.djangoproject.com/ja/2.1/ref/models/fields/
     """
 
-    # サンプル項目9 選択肢（固定）
     type_choice = (
         (1, '水'),
         (2, 'お茶'),
@@ -26,75 +25,17 @@ class Item(models.Model):
         null=True,
     )
 
-    # サンプル項目1 文字列
-    #sample_1 = models.CharField(
-    #    verbose_name='サンプル項目1 文字列',
-    #    max_length=20,
-    #    blank=True,
-    #    null=True,
-    #)
-
-    # サンプル項目2 メモ
-    #sample_2 = models.TextField(
-    #    verbose_name='サンプル項目2 メモ',
-    #    blank=True,
-    #    null=True,
-    #)
-
-    # サンプル項目3 整数
     amount = models.IntegerField(
         verbose_name='量',
         blank=True,
         null=True,
     )
 
-    # サンプル項目4 浮動小数点
-    #sample_4 = models.FloatField(
-    #    verbose_name='サンプル項目4 浮動小数点',
-    #    blank=True,
-    #    null=True,
-    #)
-
-    # サンプル項目5 固定小数点
-    #sample_5 = models.DecimalField(
-    #    verbose_name='サンプル項目5 固定小数点',
-    #    max_digits=5,
-    #    decimal_places=2,
-    #    blank=True,
-    #    null=True,
-    #)
-
-    # サンプル項目6 ブール値
-    #sample_6 = models.BooleanField(
-    #    verbose_name='サンプル項目6 ブール値',
-    #)
-
-    # サンプル項目7 日付
     date = models.DateField(
         verbose_name='日付',
         blank=True,
         null=True,
     )
-
-    # サンプル項目8 日時
-    #sample_8 = models.DateTimeField(
-    #    verbose_name='サンプル項目8 日時',
-    #    blank=True,
-    #    null=True,
-    #)
-
-
-
-
-    # サンプル項目9 選択肢（マスタ連動）
-    #sample_10 = models.ForeignKey(
-    #    User,
-    #    verbose_name='サンプル項目10_選択肢（マスタ連動）',
-    #    blank=True,
-    #    null=True,
-    #    related_name='sample_10',
-    #    on_delete=models.SET_NULL,
-    #)
 
     # 以下、管理項目
 
